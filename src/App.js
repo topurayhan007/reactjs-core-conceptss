6e1c9963d7a7f44c02d41303e0116ab64b1edf77
topurayhan007 <topurayhan007@gmail.com>
2023-03-21 19:10:01 +0600

    Added all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const number = 5555;
+const singers = [
+  { name: "Dr. Mahfuz", job: "Singer" },
+  { name: "Eva Rahman", job: "Singer2" },
+  { name: "Agun", job: "Sopno" },
+  { name: "Shuvro", job: "Pathor" },
+];
+
+const singerStyle = {
+  color: "purple",
+  backgroundColor: "white",
+};
 
 function App() {
+  const nayoks = [
+    "Rubel",
+    "BappaDa",
+    "Kuber",
+    "Joshim",
+    "Salman Shah",
+    "Riyaz",
+    "Anowar",
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {nayoks.map((nayok) => (
+        <li>Name: {nayok}</li>
+      ))}
+
+      {nayoks.map((nayok) => (
+        <Person name={nayok}></Person>
+      ))}
+
+      {singers.map((singer) => (
+        <Person name={singer.name}></Person>
+      ))}
+      <Person name={nayoks[0]} nayika="Moushumi"></Person>
+      <Person name={nayoks[1]} nayika="Cheka"></Person>
+      <Person name={nayoks[2]} nayika="Kopila"></Person>
+      <h5>New Component. YAY</h5>
+      <p id="totcompany">Rock mama React mama</p>
+      <Friend movie="Singham" phone="01777"></Friend>
+      <Friend phone="01999"></Friend>
+    </div>
+  );
+}
+
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>Nayika: {props.nayika}</p>
+    </div>
+  );
+}
+
+function Friend(props) {
+  // console.log(props);
+  return (
+    <div className="container">
+      <h3>Movie: {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
     </div>
   );
 }
